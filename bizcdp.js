ab084ed0799394e53964cd3bf5932ce6e96f69cf
wanjo <wanjo@mega.tech>
2024-03-14 12:52:44 +0800

    tricks for mjs cache

--- bizcdp.js
+++ bizcdp.js
@@ -1,3 +1,4 @@
 //to module bizcdp.mjs
-var bizcdp = async(...args)=>(await (await import('./bizcdp.mjs')).default(...args));
+var init_time = new Date().getTime();
+var bizcdp = async(...args)=>(await (await import('./bizcdp.mjs?'+init_time)).default(...args));
 if (typeof module!='undefined') module.exports = bizcdp;
