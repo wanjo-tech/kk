f8395023c1b8f95983e9df207b6d067818ae2480
wanjo <wanjo@mega.tech>
2024-03-23 21:18:00 +0800

    x

--- chrome_ext/x/content.js
+++ chrome_ext/x/content.js
@@ -1,11 +1,17 @@
 function hideUsername() {
-    if (document.href.includes('twitter.com')){
-      document.querySelectorAll('span.css-1qaijid.r-bcqeeo.r-qvutc0.r-poiln3').forEach(n=>{if(n.innerText.trim()[0]=='@')n.style.display='none'});
-    }
+  document.querySelectorAll('span.css-1qaijid.r-bcqeeo.r-qvutc0.r-poiln3').forEach(n=>{if(n.innerText.trim()[0]=='@')n.style.display='none'});
+  console.warn('YES',new Date());
 }
 
 const observer = new MutationObserver(hideUsername);
-observer.observe(document.body, { childList: true, subtree: true });
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+  characterData: true,
+});
 
 hideUsername();
 
+window.addEventListener('focus', hideUsername);
+window.addEventListener('resize', hideUsername);
