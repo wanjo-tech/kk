196067a608ffe6a99490bdc7366ebb8a45e0c2a6
wanjo <wanjo@mega.tech>
2024-03-25 02:14:10 +0000

    Sync from AIW 2024-03-25 02:14:10

--- docs/jx.js
+++ docs/jx.js
@@ -2,9 +2,9 @@
 var jx = (tbx=window.document,__='j-')=>{const VER=240324.3,
 JIF=__+'if',JELSE=__+'else',JFOR=__+'for',JTEXT=__+'text',JHTML=__+'html',JWARN=__+'warn',JERR=__+'err',
 tryx=(f,h)=>{try{return f()}catch(ex){return h?h===true?ex:h(ex):h}},
-jev=function(){with(this)return eval(arguments[0])},
-jxEval=(js,ctx)=>jev.bind(ctx)(js),
-jxTryEval=(txt,ctx,h)=>tryx(()=>jxEval(txt,ctx),h),
+jev=function(){with(arguments[1]||this)return eval(arguments[0])},
+jxEval=(js,ctx,that)=>jev.bind(that||ctx)(js,ctx),
+jxTryEval=(txt,ctx,h,that)=>tryx(()=>jxEval(txt,ctx,that),h),
 s2o=(s,h)=>jxTryEval(`(${s})`,h),
 o2s=(o,h)=>tryx(()=>JSON.stringify(o),h),
 s2frg=(s)=>(s?tbx.createRange().createContextualFragment(s):tbx.createDocumentFragment()),
@@ -17,29 +17,27 @@ _findSiblingWithAttribute=(n,a)=>{while(n=n.nextSibling){if(n.hasAttribute?.(a))
 frg4s=(s)=>(typeof(s)=='string')?(s[0]=='#'?tbx.querySelector(s):s2frg(s)):s;
 function _jxBuild(node, data={}){
   if (node.nodeType === 3)//TEXT_NODE
-    return tbx.createTextNode(node.textContent.replace(/\{\{(.*?)\}\}/g,(match,expr)=>_jxExpand(jxTryEval(expr,data,ex=>'['+ex+']'))));
+    return tbx.createTextNode(node.textContent.replace(/\{\{(.*?)\}\}/g,(m,xpr)=>_jxExpand(jxTryEval(xpr,data,ex=>'['+ex+']'))));
   if (node.tagName=='TEMPLATE') node = s2frg(node.innerHTML);
-  var returnNode = node.cloneNode(),
-    hWarn = ex=>(returnNode.setAttribute?.(JWARN,''+ex),''),
-    hErr = ex=>(returnNode.setAttribute?.(JERR,''+ex),'['+ex+']'),
-    renAttribute=(d,n,v='',n2)=>(d.removeAttribute?.(n),v && d.setAttribute?.(n2===undefined?(n+'-'):n2,v)),
+  var returnNode = node.cloneNode(),hWarn=ex=>(returnNode.setAttribute?.(JWARN,''+ex),''),
+    renAttribute=(d,n,v='',n2)=>(d.removeAttribute?.(n),v&&d.setAttribute?.(n2===undefined?(n+'-'):n2,v)),
     rebuildWith=(nn,attrName,attrVal,dt,rt)=>(rt=nn.cloneNode(true),renAttribute(rt,attrName,attrVal),_jxBuild(rt,dt));
   for(const{name,value}of[...node.attributes||[]]){
-    switch (true) {
-      case name==JFOR:
-        const match = value.match(/^\(?(\w+)(?:,\s*(\w+))?(?:,\s*(\w+))?[\)\s]?\s*in\s*(.+)$/)
-          if (!match) returnNode.setAttribute?.(JERR,'for');
-          else{
-            const [_,valVar,keyVar,idxVar,itemsStr] = match;
-            var items = jxTryEval(itemsStr,data) || {}
-            if (typeof items=='number') items = Array.from({ length: items }, (_, i) => i);
-            returnNode = s2frg();
-            Object.entries(items).forEach(([key, val], idx)=>{
-              const loopData={...data,[valVar]:val,...(keyVar&&{[keyVar]:key}),...(idxVar&&{[idxVar]:idx})};
-              returnNode.appendChild(rebuildWith(node,JFOR,value,loopData));
-            })
-          };return returnNode;
-      case name==JIF:
+      if (name==JFOR){
+        const match = value.match(/^\(?(\w+)(?:,\s*(\w+))?(?:,\s*(\w+))?[\)\s]?\s*in\s*(.+)$/);
+        if (!match) returnNode.setAttribute?.(JERR,'for');
+        else{
+          const [_,valVar,keyVar,idxVar,itemsStr] = match;
+          var items = jxTryEval(itemsStr,data) || {}
+          if (typeof items=='number') items = Array.from({ length: items }, (_, i) => i);
+          returnNode = s2frg();
+          Object.entries(items).forEach(([key, val], idx)=>{
+            const loopData={...data,[valVar]:val,...(keyVar&&{[keyVar]:key}),...(idxVar&&{[idxVar]:idx})};
+            returnNode.appendChild(rebuildWith(node,JFOR,value,loopData));
+          })
+        }
+        return returnNode;
+      } else if (name==JIF){
         var elseNode = _findSiblingWithAttribute(node,JELSE);
         if (node.parentNode){
           elseNode && node.parentNode.removeChild(elseNode);
@@ -47,19 +45,20 @@ function _jxBuild(node, data={}){
         }
         if (!!jxTryEval(value,data,hWarn)) return rebuildWith(node,JIF,value,data);
         else if (elseNode) return rebuildWith(elseNode,JELSE,value,data);
-      case name==JELSE:return s2frg();
-      case name==JTEXT:case name==JHTML:
+        else return s2frg();
+      } else if (name==JELSE) { return s2frg();
+      } else if (name==JTEXT || name==JHTML){
         renAttribute(returnNode,name,value);
-        var expand_value = _jxExpand(jxTryEval(value,data,hErr));
+        var expand_value = _jxExpand(jxTryEval(value,data,hWarn));
         returnNode[name==JTEXT?'textContent':'innerHTML'] = expand_value;
         if (node.tagName=='INPUT') returnNode.setAttribute?.('value',expand_value);
-        break;
-      case name.startsWith(':'):renAttribute(returnNode,name,jxTryEval(value,data,hErr),name.slice(1));break
-      case name.startsWith('@'):renAttribute(returnNode,name,value,'ren-'+name.slice(1));
+      } else if (name.startsWith(':')){renAttribute(returnNode,name,jxTryEval(value,data,hWarn),name.slice(1))
+      } else if (name.startsWith('@')){
+        renAttribute(returnNode,name,value,'ren-'+name.slice(1));
         var handler = function(event){ this.data = data; this.handler = handler;
-          var obj = jxTryEval(value,data,hErr);return (obj && obj.call)?tryx(()=>obj(event),hErr):obj;
-        };returnNode.addEventListener(name.slice(1),handler);break
-      }//switch
+          var obj = jxTryEval(value,data,hWarn,this);return (obj && obj.call)?tryx(()=>obj(event),hWarn):obj;
+        };returnNode.addEventListener(name.slice(1),handler);
+      }
     }//for
     node.childNodes.forEach(child=>returnNode.appendChild(_jxBuild(child,data)))
     return returnNode
@@ -85,15 +84,17 @@ function _jxUpsert(pn, nn, skipJsTag=false){
   }
   return pn
 }
-function jxMon(data, onChange){
-  const handler = {
+function jxMon(data,onChange){
+  setTimeout(onChange,1);//landing;)
+  return new Proxy(data, {
     get(target, property, receiver) {
       return tryx(()=>new Proxy(target[property], handler),(err)=>Reflect.get(target, property, receiver))
     },
-    set(target, property, value) { onChange(property, value); return Reflect.set(target, property, value); }
-  };
-  setTimeout(onChange,1);//landing;)
-  return new Proxy(data, handler);
+    set(target, property, value) {
+      setTimeout(()=>onChange(property, value, Reflect.get(target, property, target)),1);
+      return Reflect.set(target, property, value);
+    }
+  });
 }
 var jxRender=(tgt,src,data)=>_jxUpsert(tgt=frg4s(tgt),_jxBuild(src=frg4s(src),data));
 return {VER,jxRender,jxEval,jxTryEval,_jxBuild,_jxUpsert,tryx,s2o,o2s,s2bdy,s2el,s2ela,s2frg,frg2s,frg4s,jxMon}
